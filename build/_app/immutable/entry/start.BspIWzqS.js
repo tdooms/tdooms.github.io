43e96dc620b81f87f54124871a240241c20ddd20
thomas <thomas@dooms.eu>
2025-04-07 14:11:42 +0200

    add build file

---
+++ build/_app/immutable/entry/start.BspIWzqS.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/dL3SLxkk.js";export{t as start};
---
+++ build/_app/immutable/nodes/22.BI1-Evpr.js
@@ -0,0 +1,1 @@
+import{s}from"../chunks/CRYARyhg.js";import{S as t,i as e}from"../chunks/BwxSQcIe.js";class l extends t{constructor(o){super(),e(this,o,null,null,s,{})}}export{l as component};
